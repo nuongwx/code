9cd5ac4cf9a624738845c4595bebe30b9d28a7a6
nuongwx <27423086+nuongwx@users.noreply.github.com>
2023-04-17 18:07:24 +0700

    Update scheduler.c

--- test/scheduler.c
+++ test/scheduler.c
@@ -4,7 +4,7 @@ int main() // rs = 563
 {
     int pingPID, pongPID;
     PrintString("Ping-pong test starting...\n\n");
-    Exec("/home/ngwx/nachos/nachos-3.4/code/test/ping", 4);
-    Exec("/home/ngwx/nachos/nachos-3.4/code/test/pong", 4);
+    pingPID = Exec("/home/ngwx/nachos/nachos-3.4/code/test/ping", 4);
+    pongPID = Exec("/home/ngwx/nachos/nachos-3.4/code/test/pong", 4);
     Read(buffer, 1, 0);
 }
